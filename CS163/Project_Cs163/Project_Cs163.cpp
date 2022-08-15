66a78ead168d2662e728b7de22e157186cf16e1b
HarryNgo21 <minhvi176@gmail.com>
2022-08-15 12:45:15 +0700

    update

--- CS163/Project_Cs163/Project_Cs163.cpp
+++ CS163/Project_Cs163/Project_Cs163.cpp
@@ -1340,8 +1340,7 @@ void ResetToOriginal(AVL& tree, string& struct_dir, string& def_dir, string& has
     bool Check = DeleteFile(struct_dir);
     bool Check1 = DeleteFile(def_dir);
     bool Check2 = DeleteFile(hash_dir);
-    bNode* root = tree.get_root();
-    clear(root);
+    tree = AVL();
     int size;
     if (struct_dir == "database\\eng-eng\\struct.bin")
     {
